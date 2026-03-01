78bb2f41cdaaca00a2be3be87cdf9ed2f4b99781
Harmony Brown <Harmony.Brown24@yahoo.com>
2026-03-01 14:52:17 -0500

    Initial commit- Wordle2 project

---
+++ app/src/main/java/com/example/wordle2/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.wordle2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordToGuess: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun checkGuess(guess: String): String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            } else if (guess[i] in wordToGuess) {
+                result += "+"
+            } else {
+                result += "X"
+            }
+        }
+        return result
+    }
+}
